b6739c1a9159fbe37d75054d63f2c71583fb7a82
MrWooJ <hurricanc@gmail.com>
2017-04-11 20:58:39 +0430

    Handle Placement Create/Update Requests, WhiteList & Validations

--- common/models/publisher.js
+++ common/models/publisher.js
@@ -1,21 +1,83 @@
 var osList = require('../../config/operatingSystem.json')
+var statusConfig = require('../../config/status.json')
+var app = require('../../server/server')
 
 module.exports = function (publisher) {
   
   publisher.validatesInclusionOf('operatingSystem', { in: osList })
+  var statusList = []
+  for (var key in statusConfig)
+    statusList.push(statusConfig[key])
+  publisher.validatesInclusionOf('status', { in: statusList })
 
   publisher.beforeRemote('prototype.__create__placements', function (ctx, modelInstance, next) {
     if (!ctx.args.options.accessToken)
       return next()
-    ctx.args.data.minCredit = 0
-    ctx.args.data.clientId = ctx.args.options.accessToken.userId
-    next()
+    var whiteList = ['beginninTime', 'endingTime', 'name', 'style', 'status']
+    if (utility.inputChecker(ctx.args.data, whiteList)) {
+      publisher.findById(ctx.req.params.id, function (err, result) {
+        if (err)
+          throw err
+        ctx.args.data.clientId = ctx.args.options.accessToken.userId
+        ctx.args.data.minCredit = 0
+        if (result.status === statusConfig.disable) {
+          ctx.args.data.status = statusConfig.disable
+          ctx.args.data.message = 'Placement is Disabled'
+        }
+        else {
+          if (!ctx.args.data.status) {
+            ctx.args.data.status = statusConfig.Enable
+            ctx.args.data.message = 'Placement is Enable'            
+          }
+          else {
+            if (ctx.args.data.status === statusConfig.disable)
+              ctx.args.data.message = 'Placement is Disabled'
+            else if (ctx.args.data.status === statusConfig.enabled)
+              ctx.args.data.message = 'Placement is Enabled'
+          }
+        }
+        if (ctx.args.data.beginningTime < utility.getUnixTimeStamp())
+          return next(new Error('Beginning Time Can not be Less than Now'))
+        if (ctx.args.data.endingTime > ctx.args.data.beginningTime < 86400000)
+          return next(new Error('Duration Problem'))
+        return next()
+      })
+    } else
+        return next(new Error('White List Error! Allowed Parameters: ' + whiteList.toString()))
   })
 
   publisher.beforeRemote('prototype.__updateById__placements', function (ctx, modelInstance, next) {
     if (!ctx.args.options.accessToken)
       return next()
-    ctx.args.data.clientId = ctx.args.options.accessToken.userId
-    next()
+    var whiteList = ['beginninTime', 'endingTime', 'name', 'style', 'status']
+    if (utility.inputChecker(ctx.args.data, whiteList)) {
+      var placement = app.models.placement
+      placement.findById(ctx.req.params.fk, function (err, response) {
+        if (err)
+          throw err
+        if (ctx.args.data.endingTime && ctx.args.data.beginningTime) {
+          if (ctx.args.data.beginningTime < utility.getUnixTimeStamp())
+            return next(new Error('Beginning Time Can not be Less than Now'))
+          if ((ctx.args.data.endingTime - ctx.args.data.beginningTime < 86400000) || (ctx.ats.data.endingTime < utility.getUnixTimeStamp()))
+            return next(new Error('Ending Time Can not be Less than Now Or Duration Problem'))
+        }
+
+        if (!ctx.args.data.endingTime && ctx.args.data.beginningTime) {
+          if (ctx.args.data.beginningTime < utility.getUnixTimeStamp())
+            return next(new Error('Beginning Time Can not be Less than Now'))
+          if (response.endingTime - ctx.args.data.beginningTime < 86400000)
+            return next(new Error('Duration Problem'))
+        }
+
+        if (ctx.args.data.endingTime && !ctx.args.data.beginningTime) {
+          if (ctx.args.data.endingTime < utility.getUnixTimeStamp())
+            return next(new Error('Ending Time Can not be Less than Now'))
+          if (ctx.args.data.endingTime - response.beginningTime < 86400000)
+            return next(new Error('Duration Problem'))
+        }
+        next()
+      })
+    } else
+        return next(new Error('White List Error! Allowed Parameters: ' + whiteList.toString()))
   })
 }
