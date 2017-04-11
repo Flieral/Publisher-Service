3c8c07e92921010364cab4c9414c935cd1ed13a3
MrWooJ <hurricanc@gmail.com>
2017-04-11 21:23:47 +0430

    Turn AccountType JSON to Array for Inclusion Validation

--- common/models/publisherAccount.js
+++ common/models/publisherAccount.js
@@ -1,5 +1,9 @@
 var accountType = require('../../config/accountType.json')
 
 module.exports = function (publisherAccount) {
-  publisherAccount.validatesInclusionOf('type', {in: accountType})
+  var accountTypeList = []
+  for (var key in accountType) 
+    accountTypeList.push(accountType[key])
+  
+  announcerAccount.validatesInclusionOf('type', {in: accountTypeList})
 }
